8ae091d700799728647cdb68bfb7373358f9e0f1
Jay <69604238+MaybeJaybe@users.noreply.github.com>
2023-05-09 15:06:37 -0700

    finished challenges

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import Item from './Item';
+import { cats, dogs } from './breeds';
 
 export default function App() {
+  const [search, setSearch] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.kav}
+      >
+        {/* <ScrollView> */}
+        <StatusBar style="auto" />
+        {/* <TextInput style={styles.search} placeholder="Search"/> */}
+        <View style={styles.listContainer}>
+          <FlatList 
+            data={cats.filter(item => item.breed.includes(search))}
+            keyExtractor={item => item.breed}
+            renderItem={({item, index}) => {
+              return <Item index={index} data={item}/>
+            }}
+          />
+          {/* {cats.map(cat => <Item name={cat.breed} />)} */}
+          {/* <Text style={styles.heading}>Hello</Text>
+          <Text>Foo Bar</Text>
+          <Text style={styles.small}>Small Print</Text> */}
+          {/* <StatusBar style="auto" /> */}
+        </View>
+        <TextInput 
+          style={styles.search} 
+          placeholder="Search"
+          onChangeText={setSearch}
+          value={search}
+        />
+        {/* </ScrollView> */}
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  kav: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+    marginBottom: 30,
+  },
+  listContainer: {
+    width: '100%'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 50,
+    color: 'tomato',
+    fontWeight: 'bold',
+  },
+  search: {
+    fontSize: 24,
+    padding: 10,
+    borderWidth: 1,
   },
 });
---
+++ Feature.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+function Feature({ name, value }) {
+	let stars = ''
+	for (let i = 0; i < value; i += 1) {
+		stars += '★'
+	}
+
+	return (
+		<View style={styles.container}>
+			<Text style={styles.name}>{name}</Text>
+			<Text style={styles.value}>{stars}</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		padding: 8,
+	},
+	name: {
+		fontSize: 18,
+	},
+	value: {
+		fontSize: 16,
+		fontWeight: 'bold'
+	}
+})
+
+export default Feature
